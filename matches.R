b4ddae88a374d6a7287018b2664ba0aaa4fd8989
Asir <83521671+intisar291@users.noreply.github.com>
2023-03-19 14:58:47 +0100

    Add files via upload

---
+++ matches.R
@@ -0,0 +1,107 @@
+library(RSQLite)
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(forcats)
+library(lubridate)
+
+# connect to database
+db_file="F:/Study/ORBA/Visual Analytics/Exercise 2022 (Ex. 5 & 7 not given)/database/EuropeanFootball.sqlite"
+con <- dbConnect(SQLite(), dbname = db_file)
+
+# table queries
+
+match <- tibble::as_tibble(dbGetQuery(con,"SELECT * FROM Match"))
+league <-tibble::as_tibble(dbGetQuery(con,"SELECT * FROM League"))
+head(match[,1:3],n=3)
+colnames(match)
+#top 6 leagues goal comparison
+
+##top 6 team joining with league table & match table
+
+top_leagues= filter(league,name %in% list("England Premier League",
+                                    "Spain LIGA BBVA", 
+                                    "Italy Serie A",
+                                    "France Ligue 1",
+                                    "Portugal Liga ZON Sagres",
+                                    "Germany 1. Bundesliga"))
+
+top_leagues_match=inner_join(match, top_leagues %>% select(league_id = id,league_name=name), by = "league_id")
+
+## Checking the avg goals
+avg_goal_league=top_leagues_match%>%
+  group_by(league_name)%>%
+  filter(!is.na(away_team_goal) | !is.na(home_team_goal))%>%
+  summarise(avg_goals=mean(home_team_goal+away_team_goal),
+            avg_home_goal=mean(home_team_goal),
+            avg_away_goal=mean(away_team_goal))
+
+avg_goal_league%>%              
+  pivot_longer(cols = c(avg_goals, avg_home_goal, avg_away_goal),
+              names_to = "columns_names",
+              values_to = "columns_values")%>%
+  ggplot(aes(x = league_name, y = columns_values, fill = columns_names)) +
+  geom_col(position = "dodge")+
+  scale_fill_manual(values=heat.colors(3))
+
+#check the previously made pivot table meta structure in R
+avg_goal_league%>%              
+  pivot_longer(cols = c(avg_goals, avg_home_goal, avg_away_goal),
+               names_to = "columns_names",
+               values_to = "columns_values")
+
+
+
+# Comparing to otherleagues
+new_l=league %>% 
+  mutate(group_name=fct_collapse(name,top_5=c("England Premier League",
+                                   "Spain LIGA BBVA", 
+                                   "Italy Serie A",
+                                   "France Ligue 1",
+                                   "Germany 1. Bundesliga")))%>%
+  mutate(group_name=fct_other(group_name,keep="top_5",other_level="Other Leagues"))
+
+
+new_l_match=inner_join(match,new_l
+                       %>%select(league_id=id, group_name=group_name),by="league_id")
+
+new_l_match%>%
+  group_by(group_name)%>%
+  filter(!is.na(home_team_goal|!is.na(away_team_goal)))%>%
+  summarise(avg_goal=mean(home_team_goal+away_team_goal),
+            sd_goal=sd(home_team_goal+away_team_goal))%>%
+  pivot_longer(cols = c(avg_goal,sd_goal),
+               names_to = "Matrics",
+               values_to = "Values")%>%
+  ggplot(aes(x=group_name,y=Values,fill=Matrics))+
+  geom_col(position="dodge")
+
+
+# Home advantage
+
+
+
+match %>%
+  pivot_longer(cols=c(home_team_goal,away_team_goal),
+               names_to = "playing_as",
+               values_to = "number_of_goals")%>%
+  ggplot(aes(x=playing_as,y=number_of_goals))+
+  geom_violin(position = "identity",adjust=0.9
+              ,alpha=0.5,size=0.8)+
+  stat_summary(fun=median,geom="crossbar",alpha=0.25,width=0.2,)
+
+# more goal scoring months
+
+match%>%
+  mutate(match_month=month(as_date(date),label=TRUE))%>%
+  group_by(match_month)%>%
+  filter(!is.na(home_team_goal|!is.na(away_team_goal)))%>%
+  summarize(avg_goal=mean(home_team_goal+away_team_goal))%>%
+  ggplot(aes(x=match_month,y=avg_goal,group=1))+
+  geom_line(color="black")+
+  geom_point(color="red")+
+  scale_y_continuous(limits=c(0,5))
+
+dbDisconnect(con)
+
---
+++ players.R
@@ -0,0 +1,55 @@
+library(RSQLite)
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(forcats)
+library(lubridate)
+
+# connect to database
+db_file="F:/Study/ORBA/Visual Analytics/Exercise 2022 (Ex. 5 & 7 not given)/database/EuropeanFootball.sqlite"
+con <- dbConnect(SQLite(), dbname = db_file)
+
+player<- tibble::as_tibble(dbGetQuery(con,"SELECT * FROM Player"))
+player_attributes<- tibble::as_tibble(dbGetQuery(con,"SELECT * FROM Player_Attributes"))
+
+team<- tibble::as_tibble(dbGetQuery(con,"SELECT * FROM Team"))
+team_attributes<- tibble::as_tibble(dbGetQuery(con,"SELECT * FROM Team_Attributes"))
+
+# Agility vs height
+player_table= player_attributes%>%
+  mutate(playing_years=year(as_date(date)))%>%
+  inner_join(player,player_attributes,by="player_fifa_api_id")
+
+player_table%>%filter(playing_years==2015)%>%
+  ggplot(aes(x=agility,y=height))+geom_line(color="blue",size=1,alpha=0.5)+
+  scale_x_continuous(limits = c(15,100),breaks=seq(15,100,by=20))
+    
+# reaction and volly based on preferred foot  
+
+
+player_table%>%filter(playing_years==2015 & !is.na(preferred_foot))%>%
+  ggplot(aes(x=reactions,y=volleys,color=preferred_foot))+geom_point(
+                                               alpha=0.5)+
+  
+  scale_x_continuous(limits = c(30,100),breaks=seq(30,100,by=10))+
+  scale_y_continuous(limits = c(15,100),breaks=seq(15,100,by=20))
+
+
+# long pass and aggression with defensive workrate
+
+## Change the string 0-9 in to category in defensive_work_rate 
+####(some values are already in categorical form in defensive_work_rate)
+
+player_table$defensive_work_rate<-
+  ifelse(player_table$defensive_work_rate %in% c("0","1","2","3"),"low",
+         ifelse(player_table$defensive_work_rate %in% c("4","5","6","7"),"medium",
+                ifelse(player_table$defensive_work_rate %in% c("8","9"),"high",
+                       player_table$defensive_work_rate)))
+
+
+player_table%>%
+  filter(playing_years==2015 & !is.na(defensive_work_rate))%>%
+  ggplot(aes(x=long_passing,y=aggression,color=defensive_work_rate))+
+  geom_point(shape=15,alpha=0.5,size=1)
+
